cb15fd85cf43636cbccbf29aac4738e16680524a
EerajJan <eeraj.jn@googlemail.com>
2026-05-11 12:24:26 +0200

    Fix: useEffect import in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Leaderboard from './Leaderboard'
 import { AnimatedThemeToggler } from './ui/AnimatedThemeToggler'
 
